0314ddfc8fe9b78123424c0d0422df081d2d4df9
Jansen <jansenyouj@gmail.com>
2020-09-18 18:39:23 +1000

    role filter

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // Written by Jansen Malem
 // 17/9/2020
-
+import { UserSystem } from "./src/controller/UserSystem";
 // Coding Challenge - Users Hierarchy
+
+const userSystem = new UserSystem(users, roles);
+
+userSystem.getChildRoles(2);
--- src/controller/UserSystem.js
+++ src/controller/UserSystem.js
@@ -1,32 +1,69 @@
+import _ from "lodash";
 export class UserSystem {
-  constructor(users = [], roles = []) {
-    this.users = users;
-    this.roles = roles;
-  }
+	constructor(users, roles) {
+		this.users = users;
+		this.roles = roles;
+	}
 
-  getSubordinates(userId) {
-    let result = [];
-    // Iterate the roles array, filter out the unwanted roles
+	getSubordinates(userId) {
+		let result = [];
+		// Iterate the roles array, filter out the unwanted roles
 
-    // filter from users list, O(N^2) comlexity
+		// filter from users list
+		return result;
+	}
 
-    return result;
-  }
+	getChildRoles(roleId = 0) {
+		if (_.isNil(roleId)) {
+			throw "Invalid role Id";
+		}
+		if (roleId < 1) {
+			throw "Invalid role Id";
+		}
+		const roles = _.clone(this.roles);
 
-  getFilteredRoles() {}
+		const path = [];
+		const q = [];
 
-  getUsers() {
-    return this.users;
-  }
+		// Index of the currentRoot
+		let root = roles.find((e) => e["Id"] === roleId);
 
-  setUsers(users = []) {
-    this.users = users;
-  }
-  getRoles() {
-    return this.roles;
-  }
+		// current role Id
+		let rootId = root["Id"];
 
-  setRoles(roles = []) {
-    this.roles = roles;
-  }
+		q.push(rootId);
+		while (q.length > 0) {
+			// console.log(q);
+			// Dequeue
+			const currId = q.shift();
+
+			const child = roles.filter((r) => {
+				return r.Parent === currId;
+			});
+
+			child.forEach((element) => {
+				// Add child to queue
+				q.push(element["Id"]);
+				// Add child to path
+				path.push(element["Id"]);
+			});
+		}
+		console.log(path);
+		return path;
+	}
+
+	getUsers() {
+		return this.users;
+	}
+
+	setUsers(users = []) {
+		this.users = users;
+	}
+	getRoles() {
+		return this.roles;
+	}
+
+	setRoles(roles = []) {
+		this.roles = roles;
+	}
 }
